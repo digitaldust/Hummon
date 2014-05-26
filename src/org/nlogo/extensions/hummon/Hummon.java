657bbb80dc24bb75c67f40ddcb8c1916716bad5f
digitaldust <digitaldust@digitaldust.lan>
2014-05-26 15:57:04 +0200

    final version 3.0

--- src/org/nlogo/extensions/hummon/Hummon.java
+++ src/org/nlogo/extensions/hummon/Hummon.java
@@ -1,33 +1,27 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.nlogo.extensions.hummon;
 
-import edu.uci.ics.jung.algorithms.scoring.DegreeScorer;
-import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 import org.nlogo.api.DefaultClassManager;
 import org.nlogo.api.PrimitiveManager;
 
 /**
  * Replication of Hummon's "Utility and dynamic social networks".
  * @author Simone Gabbriellini
- * @version 1.0
+ * @version 3.0
  */
 public class Hummon extends DefaultClassManager {
 
     
     /**
-     * 
+     * Adds some primitives to the NetLogo environment.
      * @param primitiveManager
      */
     @Override
     public void load(PrimitiveManager primitiveManager) {
-        /** . */
+        /** reports the best move for a turtle. */
         primitiveManager.addPrimitive("step-core-model", new CoreModel());
-        /** . */
+        /** reports the degree distribution of the actual network. */
         primitiveManager.addPrimitive("degree-distribution", new DegreeDistribution());
-        /** . */
+        /** reports the best structural fit for the model when . */
         primitiveManager.addPrimitive("structural-fit", new StructuralFit());
         /** Invoke this primitive to initialize and clear everything. */
         primitiveManager.addPrimitive("initialize", new Initialize());
